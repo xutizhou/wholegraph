cb3c5d51770f8fe82ecb70605dca8ff2d581fd18
Xuting ZHOU <xutingz@xutingz-mlt.client.nvidia.com>
2024-11-13 18:36:10 +0800

    fix

--- python/pylibwholegraph/pylibwholegraph/torch/gather_fn.py
+++ python/pylibwholegraph/pylibwholegraph/torch/gather_fn.py
@@ -1,10 +1,15 @@
-import torch.nn as nn
+import torch
+from .embedding import WholeMemoryEmbedding, WholeMemoryEmbeddingModule
 
-class GatherFn(nn.Module):
-    def __init__(self, node_feat_wm_embedding):
+class GatherFn(torch.nn.Module):
+    def __init__(
+            self, 
+            node_embedding: WholeMemoryEmbedding
+    ):
         super(GatherFn, self).__init__()
-        self.node_feat_wm_embedding = node_feat_wm_embedding
+        self.node_embedding = node_embedding
+        self.gather_fn = WholeMemoryEmbeddingModule(self.node_embedding)
 
-    def forward(self, x):
-        # 定义前向传播逻辑
-        pass
+    def forward(self, ids):
+        x_feat = self.gather_fn(ids, force_dtype=torch.float32)
+        return x_feat
